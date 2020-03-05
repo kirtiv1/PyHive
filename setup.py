b2e32274463e3cf353b41fc56eaa6d198d268120
KirtiVelankar <kirti@streamsets.com>
2020-03-05 12:26:24 -0800

    Move TCLIService inside pyhive folder (#1)

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     author="Jing Wang",
     author_email="jing@dropbox.com",
     license="Apache License, Version 2.0",
-    packages=['pyhive', 'TCLIService'],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
